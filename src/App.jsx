7dbb99ed9b28af2ac765604ab5905c78943aa373
mooff <mooff@awful.cooking>
2022-09-20 13:14:15 +0000

    Tab nav working with router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import {
   BrowserRouter as Router,
-  // Switch,
+  Routes,
   Route,
-  Link
+  useMatch,
+  useResolvedPath,
+  useNavigate,
 } from "react-router-dom";
+
+import { useEffect, useRef } from 'react'
 
 import Connection from './Connection'
 import MainView from './MainView'
@@ -12,23 +16,36 @@ import MixerView from './MixerView'
 import './App.css'
 import { SlTab, SlTabGroup, SlTabPanel } from "@shoelace-style/shoelace/dist/react";
 
+function NavTab({ to, children, end=true, ...props }) {
+  const resolved = useResolvedPath(to);
+  const match = useMatch({ path: resolved.pathname, end });
+  const navigate = useNavigate()
+
+  function go() {
+    navigate(to)
+  }
+
+  return <SlTab active={match} slot="nav" onClick={go} {...props}>{children}</SlTab>
+}
+
 export default function App() {
   return <Connection>
-    <SlTabGroup>
-      <SlTab slot="nav" panel="main">
-        Main
-      </SlTab>
-      <SlTab slot="nav" panel="mixer">
-        Mixer
-      </SlTab>
+    <Router>
+      <SlTabGroup>
+        <NavTab to="/">
+          Main
+        </NavTab>
+        <NavTab to="/mixer">
+          Mixer
+        </NavTab>
+      </SlTabGroup>
 
-      <SlTabPanel name="mixer">
-        <MixerView />
-      </SlTabPanel>
-
-      <SlTabPanel name="main">
-        <MainView />
-      </SlTabPanel>
-    </SlTabGroup>
+      <Routes>
+        <Route path="/">
+          <Route path="/mixer" element={<MixerView />} />
+          <Route index element={<MainView />} />
+        </Route>
+      </Routes>
+    </Router>
   </Connection>
 }
