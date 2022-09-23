a1b2ad2f337aa9bd039e575a4c00cc05df1ccd3d
mooff <mooff@awful.cooking>
2022-09-23 01:00:28 +0000

    Improve nav buttons

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -2,6 +2,8 @@ import { Link, useLocation } from 'react-router-dom'
 
 import { Grid, Tabs, Tab, Button } from '@mui/material'
 
+import ProjectorIcon from '@mui/icons-material/DesktopMacOutlined'
+
 import useOBS from './lib/useOBS'
 
 export default function NavBar() {
@@ -11,9 +13,9 @@ export default function NavBar() {
   return <Grid container role="navigation">
     <Grid item sx={{ flexGrow: 1 }}>
       <Tabs value={location.pathname}>
-        <Tab label="Scene Editor" value="/" to="/" component={Link} />
+        <Tab label="Scenes" value="/" to="/" component={Link} />
         <Tab label="Mixer" value="/mixer" to="/mixer" component={Link} />
-        <Tab label="Projector" value="/projector" to="/projector" component={Link} />
+        <Tab label={<ProjectorIcon />} value="/projector" to="/projector" component={Link} />
       </Tabs>
     </Grid>
 
